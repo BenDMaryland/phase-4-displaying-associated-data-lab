bc00ff1df0bed8b178289a58e2a5fd171645a008
BenDMaryland <ben.d.maryland@gmail.com>
2021-11-23 17:19:51 -0500

    commit tree 0f89c10768d4d9eab851ac95afe7c0ae8208749d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,31 @@
 class ItemsController < ApplicationController
+
+
+
+
+
+
+
+def index
+item = Item.all
+render json: item, include: :user
+end
+
+
+
+    private
+
+    
+    def user_params
+    params.permit(:name, :description, :price)
+    end
+    
+    
+    
+
+
+
+
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+
+
+def show
+user = find_user
+render json: user, include: :items
+
+end
+
+
+
+
+
+private
+def find_user
+User.find(params[:id])
+end
+
+def user_params
+params.permit(:username, :city)
+end
+
+
+
+
 end
